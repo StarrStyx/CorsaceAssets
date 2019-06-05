ed770ef6accec88592980caa80a5cd293cebe1e8
Adrian <33841167+StarrStyx@users.noreply.github.com>
2019-06-05 22:11:05 +0100

    initial

---
+++ lang/cn.ts
@@ -0,0 +1,20 @@
+{
+    "flag": "https://osu.ppy.sh/images/flags/TW.png",
+    "ayim": {
+        "layout": {
+            "mca2018": "2018 MAPPER'S CHOICE AWARDS",
+            "days": "<can someone help with this I have no idea how to word this>",
+            "nav": {
+                 "standard": "standard",
+                "taiko": "taiko",
+                "fruits": "<div>catch</div> <div>the beat</div>",
+                "mania": "mania",
+                "storyboard": "storyboard"
+            }
+        },
+        "index": {
+            "homeTitle": "简介",
+            "home": "<b>A Year in Mapping</b>再度回归，为你带来2018 mapping界各种成就。 <br> <br> 跟Mapper Choice Awards一样，我们除了包括4种mode的资讯和统计数据外，今年我们还包括了storyboarding在内！ <br> <br> 还有，如果你在2018年曾经rank过一张或以上的图，我们也有为你写了几句评语。 <br> <br> 请在左边选择任何一个mode开始！"
+        }
+    }
+}
