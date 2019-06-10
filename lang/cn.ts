590dc2504ffc9b8b2bc0378aabfa7c209419c7d2
Adrian <33841167+StarrStyx@users.noreply.github.com>
2019-06-10 09:59:55 +0800

    Added days and edited home

--- lang/cn.ts
+++ lang/cn.ts
@@ -3,7 +3,7 @@
     "ayim": {
         "layout": {
             "mca2018": "2018 MAPPER'S CHOICE AWARDS",
-            "days": "<can someone help with this I have no idea how to word this>",
+            "days": "天至投票截止日期",
             "nav": {
                  "standard": "standard",
                 "taiko": "taiko",
@@ -14,7 +14,8 @@
         },
         "index": {
             "homeTitle": "简介",
-            "home": "<b>A Year in Mapping</b>再度回归，为你带来2018 mapping界各种成就。 <br> <br> 跟Mapper Choice Awards一样，我们除了包括4种mode的资讯和统计数据外，今年我们还包括了storyboarding在内！ <br> <br> 还有，如果你在2018年曾经rank过一张或以上的图，我们也有为你写了几句评语。 <br> <br> 请在左边选择任何一个mode开始！"
+            "home": "<b>A Year in Mapping(AYIM)</b>再度回归，为您带来过去的这一年里Mapping界的各种成就。<br><br>与Mapper Choice Awards(MCA)相似，我们收录了全部4种游戏模式的资讯和所统计的数据。和往年不一样的是，今年我们还包括了Storyboarding在内。<br><br>不仅如此，如果您在2018年曾经Rank过一张或以上的图，为了感谢您的贡献
+，我们还为您写了一小段话。<br><br> 请从左边选择一个游戏模式的图标开始浏览！"
         }
     }
 }
